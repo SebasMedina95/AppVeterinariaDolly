7889cd337c455c055bb3bb4013270417db937de8
Sebastian Medina <jsebastian19952011@gmail.com>
2024-07-08 23:14:43 -0500

    Primer commit

---
+++ src/main/java/com/sebastian/veterinaria_dolly/veterinaria_dolly/VeterinariaDollyApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.veterinaria_dolly.veterinaria_dolly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaDollyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaDollyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastian/veterinaria_dolly/veterinaria_dolly/VeterinariaDollyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.veterinaria_dolly.veterinaria_dolly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaDollyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
